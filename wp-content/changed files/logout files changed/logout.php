944765cd639b778c78db9b2937d525317973bb5b
Batyrzhan <zh.batyrzhan789@gmail.com>
2023-01-12 00:19:09 +0600

    added files 2

---
+++ wp-content/changed files/logout files changed/logout.php
@@ -0,0 +1,6 @@
+<a href="<?php do_shortcode('[log-out-custom]'); ?>" class="stm-lms-logout-button btn btn-default">
+    <i class="fas fa-power-off"></i>
+    <span>
+        <?php esc_html_e('Log out', 'masterstudy-lms-learning-management-system'); ?>
+    </span>
+</a>
